c1f92f17c1c5437ddd2cb42f14f48e55bb531d6a
Rutvik Raut <rutvikraut567@gmail.com>
2024-09-07 23:49:49 +0530

    Humidity values added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let weatherDescription=document.querySelector('.js-weather-dec')
 let weatherImage=document.querySelector('#weather-image')
 let minTemp=document.querySelector('.min-temp')
 let maxTemp=document.querySelector('.max-temp')
+let humidityCount=document.querySelector('.humidity-count')
+let humidityBarValue=document.querySelector('.humidity-value')
 
 const apikey='7b44ef8be9d664b20428d4d35a805526'
 
@@ -18,6 +20,7 @@ async function getWeatherDetails(){
         UpdateWeatherDetails(data.weather[0])
         UpdateCurrentTemperature(data.main)
         UpdateCurrentLocation(data.name,data.sys.country)
+        UpdateHumidity(data.main)
     }catch(error){
         console.log(error)
     }
@@ -31,11 +34,18 @@ function UpdateCurrentTemperature(tempObj){
     tempCount.innerText=Math.floor(tempObj.temp- 273.15)
     minTemp.innerText=Math.floor(tempObj.temp_min-273.15)
     maxTemp.innerText=Math.floor(tempObj.temp_max-273.15)
+    
+
 }
 
 function UpdateWeatherDetails(weather){
-    weatherDescription.innerText=weather.description
-    weatherImage.src=`http://openweathermap.org/img/wn/${weather.icon}.png`
-    
-    
+    weatherDescription.innerText=(weather.description).toUpperCase()
+    weatherImage.src=`http://openweathermap.org/img/wn/${weather.icon}.png`    
+}
+
+function UpdateHumidity(tempObj){
+    humidityCount.innerText=`${tempObj.humidity}%`
+    let bgColor=tempObj.humidity<=30?'blue':tempObj.humidity>30 && tempObj.humidity<=60?'Green':'red'
+    humidityBarValue.style.width=`${tempObj.humidity}%`
+    humidityBarValue.style.backgroundColor=bgColor
 }
