c3fdcb67a5f35652571afa011010d43cc8824fd0
Rutvik Raut <rutvikraut567@gmail.com>
2024-09-08 16:47:08 +0530

    Added weather details and alerts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import apikey from "./config.js"
+
 let currentLocation=document.querySelector('.curr-location')
 let tempCount=document.querySelector('#curr-temp-count')
 let weatherDescription=document.querySelector('.js-weather-dec')
@@ -7,25 +9,53 @@ let maxTemp=document.querySelector('.max-temp')
 let humidityCount=document.querySelector('.humidity-count')
 let humidityBarValue=document.querySelector('.humidity-value')
 
-const apikey='7b44ef8be9d664b20428d4d35a805526'
+const api=apikey
 
 document.querySelector('.js-search-button').addEventListener('click',getWeatherDetails)
 async function getWeatherDetails(){
     const location=document.querySelector('.city-input').value
-    console.log(location)
+    console.log(isNaN(location))
     try{
-        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`)
-        const data= await response.json()
-        console.log(data)
-        UpdateWeatherDetails(data.weather[0])
-        UpdateCurrentTemperature(data.main)
-        UpdateCurrentLocation(data.name,data.sys.country)
-        UpdateHumidity(data.main)
+        if(location && isNaN(location)){
+            const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api}`)
+            const data= await response.json()
+            // console.log(data)
+            if(response.status=='200'){
+                UpdateWeatherDetails(data.weather[0])
+                UpdateCurrentTemperature(data.main)
+                UpdateCurrentLocation(data.name,data.sys.country)
+                UpdateHumidity(data.main)
+                UpdateWindSpeed(data.wind.speed)
+            }
+            alertMessage(response.status)
+        }else{
+            alert("Enter value")
+        }
     }catch(error){
         console.log(error)
     }
 }
 
+function alertMessage(code){
+    const alert=document.querySelector('#alert')
+    if(code=='200'){
+        alert.classList.add('alert-success')
+        alert.classList.add('show')
+        alert.innerHTML=`<strong>Data Fetched Successfully !</strong>`
+        setTimeout(() => {
+            alert.classList.remove('alert-success')
+            alert.classList.remove('show');
+        }, 3000);
+    }else{
+        alert.classList.add('alert-danger')
+        alert.classList.add('show')
+        alert.innerHTML=`<strong>City Not Found</strong>`
+        setTimeout(() => {
+            alert.classList.remove('alert-danger')
+            alert.classList.remove('show');
+        }, 3000);
+    }
+}
 function UpdateCurrentLocation(cityName,country){
     currentLocation.value=`${cityName}, ${country}`
 }
@@ -40,12 +70,23 @@ function UpdateCurrentTemperature(tempObj){
 
 function UpdateWeatherDetails(weather){
     weatherDescription.innerText=(weather.description).toUpperCase()
-    weatherImage.src=`http://openweathermap.org/img/wn/${weather.icon}.png`    
+    weatherImage.src=`http://openweathermap.org/img/wn/${weather.icon}.png`
+    document.body.style.backgroundImage=`url(images/${weather.icon}.jpg)`
 }
 
 function UpdateHumidity(tempObj){
     humidityCount.innerText=`${tempObj.humidity}%`
-    let bgColor=tempObj.humidity<=30?'blue':tempObj.humidity>30 && tempObj.humidity<=60?'Green':'red'
-    humidityBarValue.style.width=`${tempObj.humidity}%`
+    let bgColor=tempObj.humidity<=30?'rgb(7, 143, 211)':tempObj.humidity>30 && tempObj.humidity<=60?'Green':'rgb(249, 98, 47)'
+    humidityBarValue.style.height=`${tempObj.humidity}%`
     humidityBarValue.style.backgroundColor=bgColor
+}
+
+function UpdateWindSpeed(speed){
+    let guageValueDisplay=document.querySelector('#guage-value')
+    let needle=document.querySelector('.needle')
+    const speedInKmh=Math.floor(speed*3.6)
+    guageValueDisplay.innerText=`${speedInKmh}`
+    const maxSpeed = 100;
+    const rotation = (speedInKmh / maxSpeed) * 180;
+    needle.style.transform=`rotate(${rotation}deg)`
 }
